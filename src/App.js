139bc4cbe996ea56a0b54e2413cc0a1d08397371
ikeasharks <paul.zhigarev@yandex.ru>
2024-03-09 16:35:18 +0300

    initial 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
 import './App.css';
-
-
+import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
-
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
@@ -13,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      work
+      <button onClick={onToggleButton}>toggle</button>
 
     </div>
   );
