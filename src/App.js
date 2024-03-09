9f772b03d23345656798a315685974c87ab5270e
ikeasharks <paul.zhigarev@yandex.ru>
2024-03-09 15:48:23 +0300

    initial 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, {useState, useEffect} from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
 
 
 
 function App() {
-
-  // const [data, setData] = useState(null)
-
-  // useEffect(() => {
-  //   fetch('/api')
-  //   .then(response => response.json())
-  //   .then(response => setData(response.message))
-  // }, [])
-
-  useEffect(() => {
-    tg.ready()
-  }, [])
-
-  const onClose = () => {
-    tg.close()
-  }
 
   return (
     <div className="App">
